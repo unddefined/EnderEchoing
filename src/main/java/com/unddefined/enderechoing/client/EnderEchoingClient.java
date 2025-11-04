b2c0729b8e510e7e7347365069bfa9c960857206
unddefined <669483446@qq.com>
2025-11-04 22:26:00 +0800

    接上一条push: 添加Sculk Veil后期处理效果支持
    - 引入PostChain和ResourceLocation以支持自定义着色器，在客户端初始化时加载sculk_veil.json后期处理链
    -优化了Minecraft实例的访问方式，避免重复调用

--- src/main/java/com/unddefined/enderechoing/client/EnderEchoingClient.java
+++ src/main/java/com/unddefined/enderechoing/client/EnderEchoingClient.java
@@ -11,7 +11,9 @@ import com.unddefined.enderechoing.server.registry.BlockEntityRegistry;
 import com.unddefined.enderechoing.server.registry.ParticlesRegistry;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.PostChain;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -24,18 +26,22 @@ import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+import java.io.IOException;
+
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = EnderEchoing.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = EnderEchoing.MODID, value = Dist.CLIENT)
-public class    EnderEchoingClient {
+public class EnderEchoingClient {
+    private static Minecraft mc = Minecraft.getInstance();
+    public static PostChain sculkVeilPostChain = null;
     private static final KeyMapping OPEN_TRANSPARENT_SCREEN = new KeyMapping(
             "key." + EnderEchoing.MODID + ".open_transparent_screen",
             InputConstants.KEY_R,
             "key.categories." + EnderEchoing.MODID
     );
-    
+
 
     public EnderEchoingClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
@@ -46,6 +52,15 @@ public class    EnderEchoingClient {
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            try {
+                sculkVeilPostChain = new PostChain(mc.getTextureManager(), mc.getResourceManager(), mc.getMainRenderTarget(), ResourceLocation.fromNamespaceAndPath("enderechoing", "shaders/post/sculk_veil.json"));
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
         // Register block entity renderers
         event.enqueueWork(() -> BlockEntityRenderers.register(BlockEntityRegistry.ENDER_ECHOIC_RESONATOR.get(),
                 context -> new EnderEchoicResonatorRenderer()));
@@ -59,21 +74,20 @@ public class    EnderEchoingClient {
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(OPEN_TRANSPARENT_SCREEN);
     }
-    
+
     @SubscribeEvent
-    public static void registerParticleProviders(RegisterParticleProvidersEvent event){
+    public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
         event.registerSpriteSet(ParticlesRegistry.DIRECT_MOVING_DUST.get(), ParticleDirectlyMovingDust.Provider::new);
     }
-    
+
     @SubscribeEvent
     public static void onKeyInput(InputEvent.Key event) {
         if (OPEN_TRANSPARENT_SCREEN.consumeClick()) {
             // 获取Minecraft实例
-            Minecraft minecraft = Minecraft.getInstance();
             // 确保玩家在游戏中且没有打开其他屏幕
-            if (minecraft.player != null && minecraft.screen == null) {
+            if (mc.player != null && mc.screen == null) {
                 // 打开自定义屏幕
-                minecraft.setScreen(new TransparentScreen());
+                mc.setScreen(new TransparentScreen());
             }
         }
     }
