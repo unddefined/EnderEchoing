3ce6b9df8074e725614acfa67e55bf26e942eb1b
unddefined <669483446@qq.com>
2026-01-06 11:42:41 +0800

    feat(block): 添加末影回响调谐器充能状态的纹理
    - 添加了充能纹理的mcmeta配置文件
    - 修改了EnderEchoTunerModel类以支持根据充能状态切换纹理

--- src/main/java/com/unddefined/enderechoing/client/model/EnderEchoTunerModel.java
+++ src/main/java/com/unddefined/enderechoing/client/model/EnderEchoTunerModel.java
@@ -5,14 +5,21 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedBlockGeoModel;
 
+import static com.unddefined.enderechoing.blocks.EnderEchoTunerBlock.CHARGED;
+
 public class EnderEchoTunerModel extends DefaultedBlockGeoModel<EnderEchoTunerBlockEntity> {
     private final ResourceLocation R = ResourceLocation.fromNamespaceAndPath("enderechoing", "ender_echo_tuner");
+
     public EnderEchoTunerModel() {super(ResourceLocation.fromNamespaceAndPath("enderechoing", "ender_echo_tuner"));}
+
     @Override
     public ResourceLocation getModelResource(EnderEchoTunerBlockEntity animatable) {return buildFormattedModelPath(R);}
 
     @Override
-    public ResourceLocation getTextureResource(EnderEchoTunerBlockEntity animatable) {return buildFormattedTexturePath(R);}
+    public ResourceLocation getTextureResource(EnderEchoTunerBlockEntity animatable) {
+        return animatable.getBlockState().getValue(CHARGED) ? buildFormattedTexturePath(R.withSuffix("_charged"))
+                : buildFormattedTexturePath(R);
+    }
 
     @Override
     public ResourceLocation getAnimationResource(EnderEchoTunerBlockEntity animatable) {return buildFormattedAnimationPath(R);}
