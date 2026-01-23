9cafaaae31d567a4ba5d4a6c0d05bc8b2592b5ed
unddefined <669483446@qq.com>
2026-01-23 18:40:03 +0800

    refactor(entities): 将CrystalHitProxyEntity的数据同步改为实体数据访问器
    - 替换直接字段存储为EntityDataAccessor<BlockPos>用于所有者位置
    - 使用SynchedEntityData同步所有者位置数据
    - 修改构造函数以设置实体数据而非直接字段赋值
    - 实现正确的数据序列化和反序列化方法

--- src/main/java/com/unddefined/enderechoing/entities/CrystalHitProxyEntity.java
+++ src/main/java/com/unddefined/enderechoing/entities/CrystalHitProxyEntity.java
@@ -5,6 +5,8 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.DamageTypeTags;
@@ -15,11 +17,11 @@ import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.level.Level;
 
 public class CrystalHitProxyEntity extends Entity {
-    private BlockPos ownerPos;
+    private static final EntityDataAccessor<BlockPos> OWNER_POS = SynchedEntityData.defineId(CrystalHitProxyEntity.class, EntityDataSerializers.BLOCK_POS);
 
-    public CrystalHitProxyEntity(Level level, BlockPos ownerPos ) {
+    public CrystalHitProxyEntity(Level level, BlockPos ownerPos) {
         this(EntityRegistry.CRYSTAL_HIT_PROXY.get(), level);
-        this.ownerPos = ownerPos;
+        this.entityData.set(OWNER_POS, ownerPos);
         this.setPos(ownerPos.getX() + 0.5, ownerPos.getY() + 0.5, ownerPos.getZ() + 0.5);
     }
 
@@ -43,7 +45,7 @@ public class CrystalHitProxyEntity extends Entity {
                 this.remove(RemovalReason.KILLED);
                 if (!source.is(DamageTypeTags.IS_EXPLOSION)) {
                     DamageSource d = source.getEntity() != null ? this.damageSources().explosion(this, source.getEntity()) : null;
-                    this.level().removeBlock(ownerPos, false);
+                    this.level().removeBlock(this.entityData.get(OWNER_POS), false);
                     this.level().explode(this, d, null, this.getX(), this.getY(), this.getZ(), 6.0F, false, Level.ExplosionInteraction.BLOCK);
                 }
 //                this.onDestroyedBy(source);
@@ -54,17 +56,17 @@ public class CrystalHitProxyEntity extends Entity {
 
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
-
+        builder.define(OWNER_POS, BlockPos.ZERO);
     }
 
     @Override
-    protected void readAdditionalSaveData(CompoundTag compoundTag) {
-
+    protected void readAdditionalSaveData(CompoundTag tag) {
+        if (tag.contains("OwnerPos")) this.entityData.set(OWNER_POS,BlockPos.of(tag.getLong("OwnerPos")));
     }
 
     @Override
-    protected void addAdditionalSaveData(CompoundTag compoundTag) {
-
+    protected void addAdditionalSaveData(CompoundTag tag) {
+        tag.putLong("OwnerPos", this.entityData.get(OWNER_POS).asLong());
     }
 
     public static class NoopRenderer extends EntityRenderer<CrystalHitProxyEntity> {
